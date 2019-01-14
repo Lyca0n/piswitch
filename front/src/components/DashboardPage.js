12405ae86e3536eba7e7c0958e283f45dcb969b1
Villalva <Julio.Villalva@autozone.com>
2019-01-14 10:13:59 -0700

    housekeeping

--- front/src/components/DashboardPage.js
+++ front/src/components/DashboardPage.js
@@ -6,7 +6,7 @@ const DashboardPage = () => (
   <div>
   <div className="page-header">
     <div className="content-container">
-      <h1 className="page-header__title">Switch List</h1>
+      <h1 className="page-header__title">Appliance List</h1>
       <div className="page-header__actions">
         <Link className="button" to="/add">Add Switch</Link>
       </div>
